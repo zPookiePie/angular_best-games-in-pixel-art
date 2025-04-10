f0647280c132dadc1d1a1ce38ed09cd651d9ef94
Ágata Rafaela <agatadevv@gmail.com>
2025-04-10 09:18:10 -0300

    feat: adiciona suporte ao SSR com prerender de rotas dinâmicas

--- blog-angular/src/app/app.routes.server.ts
+++ blog-angular/src/app/app.routes.server.ts
@@ -1,6 +1,19 @@
+// app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'content/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' }
+      ];
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
--- blog-angular/src/app/app.routes.ts
+++ blog-angular/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
-import { getPrerenderParams } from '../prerender.config';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Rota padrão
-  { path: 'content/:id', component: ContentComponent,
-    data: {
-      getPrerenderParams
-    }
+  { path: 'content/:id', component: ContentComponent
    } // Rota /content
 ];
